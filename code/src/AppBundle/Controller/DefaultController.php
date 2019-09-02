b8caaac601e49de67b89a439e53201d7c2e1860c
Vaidas <vaidas.ruskys@gmail.com>
2019-09-02 20:28:31 +0300

    datadog

--- code/src/AppBundle/Controller/DefaultController.php
+++ code/src/AppBundle/Controller/DefaultController.php
@@ -13,6 +13,9 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $logger = $this->get('stock_importer.logger.data_dog');
+        $logger->info('Index page visited', [$request->getContent()]);
+        //DD_API_KEY=1f347f60d3f61795fba852d75516f7c7 bash -c "$(curl -L https://raw.githubusercontent.com/DataDog/datadog-agent/master/cmd/agent/install_script.sh)"
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
